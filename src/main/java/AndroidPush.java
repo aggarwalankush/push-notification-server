033d5ea248e9cd6a7dbd5091f12de141a50bbf29
annaggarwal <annaggarwal@paypal.com>
2016-09-20 09:07:24 -0700

    updated from GCM to FCM

--- src/main/java/AndroidPush.java
+++ src/main/java/AndroidPush.java
@@ -6,34 +6,34 @@ import java.net.URL;
 public class AndroidPush {
 
     /**
-     * Replace API_KEY with your API_KEY
+     * Replace API_KEY with your API_KEY/SENDER_ID
      * Replace DEVICE_TOKEN with your DEVICE_TOKEN
      */
-    private static String API_KEY = "YOUR_API_KEY";
+    private static String API_KEY = "YOUR_API_KEY OR SENDER_ID";
     private static String DEVICE_TOKEN = "YOUR_DEVICE_TOKEN";
 
     public static void main(String[] args) throws Exception {
-        // Prepare JSON containing the GCM message content. What to send and where to send.
-        JSONObject jGcmData = new JSONObject();
+        // Prepare JSON containing the FCM message content. What to send and where to send.
+        JSONObject jFcmData = new JSONObject();
         JSONObject jData = new JSONObject();
         jData.put("title", "My First Notification");
         jData.put("message", "Hello, I'm push notification");
 
-        jGcmData.put("to", DEVICE_TOKEN);
-        // What to send in GCM message.
-        jGcmData.put("data", jData);
+        jFcmData.put("to", DEVICE_TOKEN);
+        // What to send in FCM message.
+        jFcmData.put("data", jData);
 
-        // Create connection to send GCM Message request.
-        URL url = new URL("https://gcm-http.googleapis.com/gcm/send");
+        // Create connection to send FCM Message request.
+        URL url = new URL("https://fcm.googleapis.com/fcm/send");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestProperty("Authorization", "key=" + API_KEY);
         conn.setRequestProperty("Content-Type", "application/json");
         conn.setRequestMethod("POST");
         conn.setDoOutput(true);
 
-        // Send GCM message content.
+        // Send FCM message content.
         OutputStream outputStream = conn.getOutputStream();
-        outputStream.write(jGcmData.toString().getBytes());
+        outputStream.write(jFcmData.toString().getBytes());
 
         System.out.println(conn.getResponseCode());
         System.out.println(conn.getResponseMessage());
