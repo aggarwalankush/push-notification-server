cc405339e789239e961524c918662e8f8607ee6a
Ankush Aggarwal <ankushagg93@gmail.com>
2016-09-21 07:28:20 -0700

    updated GCM to FCM

--- src/main/java/AndroidPush.java
+++ src/main/java/AndroidPush.java
@@ -6,10 +6,10 @@ import java.net.URL;
 public class AndroidPush {
 
     /**
-     * Replace API_KEY with your API_KEY/SENDER_ID
+     * Replace SERVER_KEY with your SERVER_KEY generated from FCM
      * Replace DEVICE_TOKEN with your DEVICE_TOKEN
      */
-    private static String API_KEY = "YOUR_API_KEY OR SENDER_ID";
+    private static String SERVER_KEY = "YOUR_SERVER_KEY";
     private static String DEVICE_TOKEN = "YOUR_DEVICE_TOKEN";
 
     public static void main(String[] args) throws Exception {
@@ -26,7 +26,7 @@ public class AndroidPush {
         // Create connection to send FCM Message request.
         URL url = new URL("https://fcm.googleapis.com/fcm/send");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestProperty("Authorization", "key=" + API_KEY);
+        conn.setRequestProperty("Authorization", "key=" + SERVER_KEY);
         conn.setRequestProperty("Content-Type", "application/json");
         conn.setRequestMethod("POST");
         conn.setDoOutput(true);
